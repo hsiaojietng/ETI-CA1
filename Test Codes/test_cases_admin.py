50c6ef66569e1fd73b0d050315bf4276a3d5d08e
Hsiao Jiet Ng <s10177814@connect.np.edu.sg>
2019-11-16 17:02:48 +0800

    Test cases for admin page

---
+++ Test Codes/test_cases_admin.py
@@ -0,0 +1,20 @@
+import pytest
+from SRC.pytest import *
+
+def test_CreateCategory:
+    assert "added successfully"
+
+def test_DeleteCategory:
+    assert "deleted successfully"
+
+def test_EditCategory
+    assert "changed successfully"
+
+def test_CreateBlog:
+    assert "added successfully"
+
+def test_DeleteBlog:
+    assert "deleted successfully"
+    
+def test_EditBlog
+    assert "changed successfully"
