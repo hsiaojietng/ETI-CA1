37e1c0fdabf838ced09e81b225ffb6778df78a95
Hsiao Jiet Ng <s10177814@connect.np.edu.sg>
2019-11-16 15:05:47 +0800

    Created displaying pytest test cases

---
+++ Test Codes/test_cases_portfolio.py
@@ -0,0 +1,30 @@
+import pytest
+from SRC.pytest import *
+
+def test_NavToHome():
+    assert "Blog Index"
+
+def test_NavToBlog():
+    assert "Projects I Have Done"
+
+def test_NavToComment():
+    assert "Leave a comment!"
+
+def test_NavToBlogPages():
+    #One of the blog page will be Resume
+    assert "Resume"
+
+def test_NavToProjectPages():
+    assert "About the project"
+
+def test_ViewResume
+    assert "Resume"
+
+def test_ViewSelfIntro
+    assert "Self-Intro"
+
+def test_ViewHobbies
+    assert "Hobbies"
+
+def test_ViewCCA:
+    assert "CCAs"
